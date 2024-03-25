f9971c5c032ea7bca9aa2d88e687174c84e92415
kuanrenqian <kuanrenqian@outlook.com>
2024-03-24 21:46:54 -0400

    locally refined version with reduced mesh density in the domain except for interface

---
+++ 2D_solver_fine/main.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include "NeuronGrowth.h"
+// #include "UserSetting.h"
+#include <sstream>
+#include <iomanip>
+#include "time.h"
+#include "utils.h"
+
+using namespace std;
+
+static char help[] = "Solve 2D Neuron Growth\n";
+
+int main(int argc, char **argv)
+{
+	int rank, nProcs;
+	PetscErrorCode ierr;
+	/// start up petsc
+	ierr = PetscInitialize(&argc, &argv, (char*)0, help); if (ierr) return ierr;
+	ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank); CHKERRQ(ierr);
+	ierr = MPI_Comm_size(PETSC_COMM_WORLD, &nProcs); CHKERRQ(ierr);
+
+	int numNeuron = atoi(argv[1]);		// user input number of neurons
+	int end_iter = atoi(argv[2]);		// user input number of iterations
+	string path_in = argv[3];		// user input working directory
+
+	int NX, NY, originX(0), originY(0);
+	vector<array<float, 2>> seed; 
+	InitializeSoma(numNeuron, seed, NX, NY);
+
+	/// Set simulation parameters and mesh
+	string fn_mesh_initial(path_in + "controlmesh_initial.vtk");
+	string fn_mesh(path_in + "controlmesh.vtk");
+	string fn_mesh_fine(path_in + "controlmesh_fine.vtk");
+	string fn_bz(path_in + "bzmeshinfo.txt.epart." + to_string(nProcs));
+	string path_out(path_in + "outputs/");
+	// string path_out(path_in);
+
+	int n_bzmesh;
+	vector<vector<float>> vertices;
+	vector<vector<int>> elements;
+	vector<vector<int>> ele_process;
+	vector<Vertex2D> cpts_initial, cpts, cpts_fine, prev_cpts;
+	vector<Element2D> tmesh_initial, tmesh, tmesh_fine;
+	ele_process.resize(nProcs);
+	PetscPrintf(PETSC_COMM_WORLD, "Initial element process size: %ld\n", ele_process.size()); CHKERRQ(ierr);
+
+	vector<int> rfid, rftype; // list of elements to refine
+	vector<vector<float>> NGvars; // for passing neuron growth variables in-between domain expansion
+	NGvars.clear(); NGvars.resize(6);
+
+	bool localRefine = false;
+	// UserSetting *NGuser = new UserSetting;
+
+	int iter(0), state(1); // 0-end, 1-running, 2-expanding, 3-diverging
+	while (iter <= end_iter) {
+		prev_cpts = cpts; // back up old control points for later NGvars interpolations (old cpts to new cpts)
+		cpts_initial.clear(); tmesh_initial.clear(); 
+		cpts.clear(); tmesh.clear(); 
+		ele_process.clear();
+		ele_process.resize(nProcs);
+
+		if (rank == 0) {
+			// to make sure correct files are generated and then read later on
+			std::remove("../io/controlmesh.vtk");
+			std::remove("../io/controlmesh_initial.vtk");
+			std::remove("../io/controlmesh_fine.vtk");
+			std::remove("../io/bzpt.txt");
+			std::remove("../io/cmat.txt");
+			std::remove("../io/bzmesh.vtk");
+			std::remove("../io/bzmeshinfo.txt");
+			string epart("../io/bzmeshinfo.txt.epart." + std::to_string(nProcs));
+			string npart("../io/bzmeshinfo.txt.npart." + std::to_string(nProcs));
+			std::remove(epart.c_str());
+			std::remove(npart.c_str());
+
+			gen2Dmesh(originX, originY, NX, NY, vertices, elements, 1); // Generating 2D quad mesh
+			write_quad_toVTK(fn_mesh_initial.c_str(), vertices, elements);
+
+			gen2Dmesh(originX, originY, NX*2, NY*2, vertices, elements, 2); // Generating 2D quad mesh
+			write_quad_toVTK(fn_mesh_fine.c_str(), vertices, elements);
+			
+			// localRefine = false;
+			if (localRefine != true) {
+				write_quad_toVTK(fn_mesh.c_str(), vertices, elements);
+				bzmesh2D(path_in); // generating 2D bezier mesh information (bzmeshinfo.txt)
+			} else {
+				ReadMesh(fn_mesh_initial, cpts_initial, tmesh_initial);
+				vector<float> tmp = InterpolateVars_new(NGvars[0], prev_cpts, cpts_initial, 1);
+				
+				string varName = "check";
+				VisualizeVTK_ControlMesh(cpts_initial, tmesh_initial, 0, path_out, tmp, varName); // solution on control points
+
+				// vector<float> tmp = interpolateValues_closest(NGvars[0], prev_cpts, cpts_initial);
+				// PrintVec2TXT(tmp, "../io2D/tmp_0.txt", 1);
+				// vector<float> tmp = interpolateValues_averageN(NGvars[0], prev_cpts, cpts_initial, 4);
+				std::cout << tmp.size() << " " << cpts_initial.size() << " " << NX << " " << NY << std::endl;
+				// ObtainRefineID(tmp, cpts_initial, NX*2, NY*2, originX, originY, rfid, rftype);
+				ObtainRefineID_new(tmp, cpts_initial, NX, NY, originX/2, originY/2, rfid, rftype);
+				THS2D(path_in, rfid, rftype);
+			}
+			mpmetis(nProcs, path_in); // partitioning bzmeshinfo using mpmetis for parallelization
+		}
+		ierr = MPI_Barrier(PETSC_COMM_WORLD); CHKERRQ(ierr);
+
+		ReadMesh(fn_mesh_initial, cpts_initial, tmesh_initial);
+		ReadMesh(fn_mesh, cpts, tmesh);
+		ReadMesh(fn_mesh_fine, cpts_fine, tmesh_fine);
+		AssignProcessor(fn_bz, n_bzmesh, ele_process);
+		PetscPrintf(PETSC_COMM_WORLD, "Processor Assigned!----------------------------------------------------------\n");
+	
+		state = RunNG(n_bzmesh, ele_process, cpts_initial, tmesh_initial, cpts_fine, tmesh_fine, cpts, prev_cpts, tmesh, path_in, path_out,
+			iter, end_iter, NGvars, NX, NY, seed, originX, originY, rfid, rftype, localRefine);
+
+		if (state == 3) {
+			PetscPrintf(PETSC_COMM_WORLD, "Simulation diverged!---------------------------------------------------------\n");
+			return 0;
+		}
+	}
+
+	PetscPrintf(PETSC_COMM_WORLD, "Done - Main reached end iteration!\n");
+	ierr = PetscFinalize(); CHKERRQ(ierr);
+	return 0;
+}
---
+++ 2D_solver_fine/utils.h
@@ -0,0 +1,90 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <array>
+#include <algorithm>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include "BasicDataStructure.h"
+
+#include <png.h>
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
+
+// Creating 2D mesh (incrementing from lo to hi)
+void gen2Dmesh(int Nx, int Ny, vector<vector<float>>& vertices, vector<vector<int>>& elements);
+void gen2Dmesh(int originX, int originY, int Nx, int Ny, vector<vector<float>>& vertices, vector<vector<int>>& elements, float ratio);
+void gen2Dmesh_new(int originX, int originY, int Nx, int Ny, vector<vector<float>>& vertices, vector<vector<int>>& elements, float ratio);
+
+// Export quad mesh to vtk for visualization
+void write_quad_toVTK(const char* qs, vector<vector<float>>& vertices, vector<vector<int>>& elements);
+
+// Creating 3D mesh (incrementing from lo to hi)
+void gen3Dmesh(int Nx, int Ny, int Nz, vector<vector<float>>& vertices, vector<vector<int>>& elements);
+
+// Export hex mesh to vtk for visualization
+void write_hex_toVTK(const char* qs, vector<vector<float>>& vertices, vector<vector<int>>& elements);
+
+void PrintVec2TXT(const vector<float>& v, string fn, bool visualization); // print out to commandline for debugging
+
+// generating 2D bezier mesh using spline2D_src
+void bzmesh2D(string path_in);
+// generating 3D bezier mesh using spline_src
+void bzmesh3D();
+
+// partitioning mesh using mpmetis
+void mpmetis(int n_process, string path_in);
+
+void THS2D(string path_in, vector<int> rfid, vector<int> rftype);
+
+void InitializeSoma(int numNeuron, vector<array<float, 2>> &seed, int &NX, int &NY);
+
+// void InitializeNGvars(vector<Vertex2D> cpts, vector<array<int, 2>> seed, vector<vector<float>> &NGvars, vector<float> &phi_0, vector<float> &tub_0, vector<float> &Mphi, int n);
+
+// Local refinement based on phi interface
+vector<float> ConvertTo1DFloatVector(const vector<vector<int>> input);
+vector<float> ConvertTo1DFloatVector(const vector<vector<float>> input);
+
+bool SearchPair(const vector<Vertex2D> prev_cpts, float targetX, float targetY, int &ind);
+bool SearchPair_debug(const vector<Vertex2D> prev_cpts, float targetX, float targetY, int &ind);
+
+vector<float> InterpolateVars(vector<vector<int>> input, vector<Vertex2D> cpts_initial, vector<Vertex2D> cpts, int type);
+vector<float> InterpolateVars(vector<float> input, vector<Vertex2D> cpts_initial, vector<Vertex2D> cpts, int type);
+float round5(float input);
+
+void VisualizeVTK_ControlMesh(const vector<Vertex2D> &spt, const vector<Element2D> &mesh, int step, string fn, vector<float> var, string varName);
+
+vector<float> InterpolateVars_new(vector<float> input, vector<Vertex2D> cpts_initial, vector<Vertex2D> cpts, int type);
+
+std::vector<float> interpolateValues_closest(
+    const std::vector<float>& phi,
+    const std::vector<Vertex2D>& cpt,
+    const std::vector<Vertex2D>& cpt_out);
+std::vector<float> interpolateValues_averageN(
+    const std::vector<float>& phi,
+    const std::vector<Vertex2D>& cpt,
+    const std::vector<Vertex2D>& cpt_out,
+    float numAverage);
+std::vector<float> interpolateValuesWithinRadius(
+    const std::vector<float>& phi,
+    const std::vector<Vertex2D>& cpt,
+    const std::vector<Vertex2D>& cpt_out,
+    float radius);
+
+float distance_d(const Vertex2D& a, const Vertex2D& b);
+
+void ObtainRefineID(vector<float> phi, vector<Vertex2D> cpts, int NX, int NY, int originX, int originY, vector<int> &rfid, vector<int> &rftype);
+void ObtainRefineID_new(vector<float> phi, vector<Vertex2D> cpts, int NX, int NY, int originX, int originY, vector<int> &rfid, vector<int> &rftype);
+
+void VisualizeVTK_ControlMesh(const vector<Vertex2D> &spt, const vector<Element2D> &mesh, int step, string fn, vector<float> var, string varName);
+
+void ReadMesh(string fn, vector<Vertex2D>& pts, vector<Element2D>& mesh); //need vtk file with point label
+
+void AssignProcessor(string fn, int &n_bzmesh, vector<vector<int>> &ele_process);
+
+void WriteMatrixToPNG(const std::vector<float>& phi, int width, int height, const char* filename);
+
+#endif
